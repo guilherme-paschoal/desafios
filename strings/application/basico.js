241ff4466eef8062385dd237929e3e3c15cd8243
guilherme-paschoal <guilherme@hgdata.com>
2017-12-04 22:49:06 -0200

    oops

--- strings/application/basico.js
+++ strings/application/basico.js
@@ -31,5 +31,5 @@ function definirComprimentoDaLinha(texto, pos, comprimento) {
 let entrada = 'In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.';
 let entrada2 = 'And God said, "Let there be light," and there was light. God saw that the light was good, and he separated the light from the darkness. God called the light "day," and the darkness he called "night." And there was evening, and there was morning - the first day.';
 
-
-//console.log(basico(entrada2, 40));
+console.log(basico(entrada1, 40));
+console.log(basico(entrada2, 40));
